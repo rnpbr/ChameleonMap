5b419cadd30d7f1c71241ba74e4feb7ef87439b8
NarcisoFilho <narcisofilho1@hotmail.com>
2026-07-08 22:45:22 -0300

    feat: add CSBC2026 visual identity and new sources

--- map-navigation-app/sources.ts
+++ map-navigation-app/sources.ts
@@ -1,8 +1,8 @@
 export const Sources = [
     "https://mapas.rnp.br/",
-    "https://mapatestbeds.rnp.br/",
-    "https://mapa.observatorioblockchain.org.br/",
     "https://cybersecmap.rnp.br/",
-    "http://localhost"
-    
+    "https://mapatestbeds.rnp.br/",
+    "https://openran.mapa.rnp.br/",
+    "https://mapaobservatorioquantico.testbeds.rnp.br/",
+    "https://mapa.observatorioblockchain.org.br/",
 ]
